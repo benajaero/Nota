d4144ae92f0075e7da0ebf8ff85a9141e9bb99dc
Benny <blaze349@protonmail.com>
2017-05-12 08:26:28 +0000

    Fixed issue...
    By replacing '() =>' with 'function()' strange because I'm running on the latest chrome engine

--- scripts/converter.js
+++ scripts/converter.js
@@ -3,11 +3,13 @@ Each time input
 convert textarea text with marked (should we do all the text or only new text)
 to other side
 */
-var marked = require('marked')
+$(document).ready(() => {
+    var marked = require('marked')
 
-var input = document.getElementById('input')
-var preview = document.getElementById('preview')
-
-input.addEventListener('input', () => {
-    preview.innerHTML = marked(input.value)
+    var input = document.getElementById('input')
+    var preview = document.getElementById('preview')
+    
+    input.addEventListener('input', () => {
+        preview.innerHTML = marked(input.value)
+    })
 })
--- scripts/index.js
+++ scripts/index.js
@@ -1,94 +1,98 @@
 
-let fs = require('fs')
-let parseJSON = require('json-parse-async')
-let randomstring = require('randomstring')
-
-let sidelist = document.getElementById('sidelist')
-sidelist.innerHTML = ""
-let button = document.getElementById('addnota')
-let titleInput = document.getElementById('title')
-
-
-//I'll use this to store the note so that I can pass in the functions
-let currentNota = null
-
-let notae = {}
-
-titleInput.addEventListener('keydown', (e) => {
-    if (e.keyCode == 13) {
-        console.log("Key code 13")
-        name(currentNota.id, titleInput.value)
-    }
-})
-
-
-
-
-fs.readFile('./notae.json', 'utf-8', (err, data) => {
-    if (err) throw err
-    parseJSON(data, (err, datae) => {
-        if (err) throw err
-        notae = datae
-        currentNota = notae.notae[0]
-        loadSidebar(notae)
-    })
-})
-
-
-button.addEventListener('click', () => {
-    let obj = {
-        name: '',
-        date: '',
-        file: randomstring.generate() + '.md',
-        id: notae.notae.length
-    }
+$(document).ready(() => {
+    let fs = require('fs')
+    let parseJSON = require('json-parse-async')
+    let randomstring = require('randomstring')
     
-    fs.writeFile(`./notae/${obj.file}`, "# This is an empty file", 'utf-8', err => {
-        if (err) throw err
+    let sidelist = document.getElementById('sidelist')
+    sidelist.innerHTML = ""
+    let button = document.getElementById('addnota')
+    let titleInput = document.getElementById('title')
+    
+    
+    //I'll use this to store the note so that I can pass in the functions
+    let currentNota = null
+    
+    let notae = {}
+    
+    titleInput.addEventListener('keydown', (e) => {
+        if (e.keyCode == 13) {
+            console.log("Key code 13")
+            name(currentNota.id, titleInput.value)
+        }
     })
     
-    notae.notae.push(obj)
-    writeMetadata()
-})
-
-function loadSidebar(notae) {
-    sidelist.innerHTML = ''
-    for (var i = 0; i < notae.notae.length; i++) {
-        sidelist.innerHTML = sidelist.innerHTML + `<li class='sideitem'> <h5>${notae.notae[i].name}</h5> <p> ${notae.notae[i].date} </p> </li>`
-    }
-    $('.sideitem').click(() => {
-        
-        $('.sideitem').removeClass('active')
-        $(this).addClass('active')
-        
-        var index = $(this).index()
-        console.log(index)
-        currentNota = notae.notae[index]
-    })
-}
-
-
-function writeMetadata() {
-    console.log("Writing metadata")
-    fs.writeFile('./notae.json', JSON.stringify(notae, null, 4), 'utf-8', (err) => {
+    
+    
+    
+    fs.readFile('./notae.json', 'utf-8', (err, data) => {
         if (err) throw err
-        console.log("Written metadata")
+        parseJSON(data, (err, datae) => {
+            if (err) throw err
+            notae = datae
+            currentNota = notae.notae[0]
+            loadSidebar(notae)
+        })
     })
-    loadSidebar(notae)
-}
-
-function name(nota, newVal) {
-    console.log("Renaming")
-    notae.notae[nota].name = newVal
-    renameFile(nota)
-    writeMetadata()
-}
-
-function renameFile(nota) {
-    let newName = notae.notae[nota].name.split(' ').join('-')
-    fs.rename(`./notae/${notae.notae[nota].file}.md`, `./notae/${newName}.md`, err => {
-        if (err) throw err
-        notae.notae[nota].file = newName
+    
+    
+    button.addEventListener('click', () => {
+        let obj = {
+            name: '',
+            date: '',
+            file: randomstring.generate() + '.md',
+            id: notae.notae.length
+        }
+        
+        fs.writeFile(`./notae/${obj.file}`, "# This is an empty file", 'utf-8', err => {
+            if (err) throw err
+        })
+        
+        notae.notae.push(obj)
         writeMetadata()
     })
-}
+    
+    function loadSidebar(notae) {
+        $('#sidelist').empty()
+        for (var i = 0; i < notae.notae.length; i++) {
+            var string = '<li class="sideitem"> <h5>' + notae.notae[i].name + '</h5> <p>' + notae.notae[i].date + '</p> </li>\n'
+            console.log(string)
+            $('#sidelist').append(string)
+        }
+        
+        $('.sideitem').click(function(){
+            var index = $(this).index()
+            console.log(index)
+            currentNota = notae.notae[index]
+            
+            $('.sideitem').removeClass('active')
+            $(this).addClass('active')
+        })
+    }
+    
+    
+    function writeMetadata() {
+        console.log("Writing metadata")
+        fs.writeFile('./notae.json', JSON.stringify(notae, null, 4), 'utf-8', (err) => {
+            if (err) throw err
+            console.log("Written metadata")
+        })
+        loadSidebar(notae)
+    }
+    
+    function name(nota, newVal) {
+        console.log("Renaming")
+        notae.notae[nota].name = newVal
+        renameFile(nota)
+        writeMetadata()
+    }
+    
+    function renameFile(nota) {
+        let newName = notae.notae[nota].name.split(' ').join('-')
+        fs.rename(`./notae/${notae.notae[nota].file}.md`, `./notae/${newName}.md`, err => {
+            if (err) throw err
+            notae.notae[nota].file = newName
+            writeMetadata()
+        })
+    }
+})
